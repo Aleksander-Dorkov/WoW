bed56d8f2391cd67f1772dbff97029d981779592
Aleksander-D-92 <69743625+Aleksander-D-92@users.noreply.github.com>
2022-12-04 13:50:23 +0200

    Update ArenaNumbers.lua

--- ArenaNumbers/ArenaNumbers.lua
+++ ArenaNumbers/ArenaNumbers.lua
@@ -1,13 +1,3 @@
-local U=UnitIsUnit hooksecurefunc("CompactUnitFrame_UpdateName",function(F)
-    if IsActiveBattlefieldArena()and F.unit:find("nameplate")
-        then for i=1,5 do if U(F.unit,"arena"..i)
-            then F.name:SetText(i)F.name:SetTextColor(1,1,0)
-                break 
-            end 
-        end 
-    end 
-end)
-
 hooksecurefunc("CompactUnitFrame_UpdateName", function(frame)
     if ShouldShowName(frame) then
         SetCVar("nameplateMinAlpha", 1);
